c5a2065ac600eabe9362d6fbdc3e0085da271849
Gonzalo <gonchizurak@gmail.com>
2021-10-08 14:59:06 -0300

    Se termina el desafio 2

--- Desafio2.js
+++ Desafio2.js
@@ -7,7 +7,7 @@ class Contenedor {
     }
     async save(data){
         data.id = this.id
-        fs.promises.readFile(this.archivo,'utf-8')
+        await fs.promises.readFile(this.archivo,'utf-8')
         .then(arch => JSON.parse(arch))// si lee el archivo lo parsea
         .then(arch => {
             if (arch.length != 0){
@@ -18,48 +18,61 @@ class Contenedor {
             return arch;
         })// si hay un array de objetos agrega el nuevo objeto
         .then(arch => {
-            fs.promises.writeFile(this.archivo, JSON.stringify(arch)).catch(error =>console.log(error));
+            fs.promises.writeFile(this.archivo, JSON.stringify(arch)).catch(error =>console.log(error))
         })// y vuelve a escribir el archivo
         .catch(error => {
             console.log(error);
             fs.promises.writeFile(this.archivo, JSON.stringify([data])).catch(err => console.log(err));
             console.log('se creo el archivo')
+            this.id = null;        
         })
-        .finally(() => {return this.id})
-
+        return this.id;
     }
 
     async getById(number){
-        try {
-            const Data = await fs.promises.readFile(this.archivo,'utf-8');
-            console.log(Data.split(','))
-            const Obj = JSON.parse(Data)
-            const Obj2 = JSON.parse(Data)
-            console.log(Obj)
-
-            console.log(Obj2)
-
-        } catch (error) {
-           console.log(error) 
-        }
-        
-        
+        return await fs.promises.readFile(this.archivo,'utf-8')
+        .then(arch => JSON.parse(arch))// si lee el archivo lo parsea
+        .then(arch => arch.filter(obj => obj.id == number))
+        .catch(error => console.log(error))
+    }
+    async getAll(){
+        return await fs.promises.readFile(this.archivo,'utf-8')
+        .then(arch => JSON.parse(arch))// si lee el archivo lo parsea
+        .catch(error => console.log(error))
+    }
+    async deleteById(number){
+        await fs.promises.readFile(this.archivo,'utf-8')
+        .then(arch => JSON.parse(arch))// si lee el archivo lo parsea
+        .then(arch => arch.filter(obj => obj.id != number))
+        .then(async arch => {
+            await fs.promises.writeFile(this.archivo, JSON.stringify(arch)).catch(error =>console.log(error))
+        })
+        .catch(error => console.log(error))
+    }
+    async deleteAll(number){
+        await fs.promises.writeFile(this.archivo, JSON.stringify([])).catch(error =>console.log(error))
     }
 }
 
-contenedor = new Contenedor('C:\\Users\\Gonzalo\\Desktop\\CursoCoder\\backendCoder\\JSON');
-contenedor.save({title: 'queso', price: 50,thumbnail: 'foto'});
-
-// contenedor.save({title: 'pan', price: 50,thumbnail: 'foto'});
-
-setTimeout(() =>{
-    contenedor.save({title: 'jamon', price: 50,thumbnail: 'foto'});
-}, 1)
+contenedor = new Contenedor('C:\\Users\\Gonzalo\\Desktop\\CursoCoder\\backendCoder\\Test.txt');
 
 
 
 
-// setTimeout(() =>{
-//     console.log(contenedor.getById(0))
-// }, 1000)
+
+const test = async () => {
+    contenedor = new Contenedor('C:\\Users\\Gonzalo\\Desktop\\CursoCoder\\backendCoder\\Test.txt');
+    for (let i = 0; i < 30; i++){
+        console.log( await contenedor.save({title: i, price: 50,thumbnail: 'foto'}))
+    }
+    console.log( await contenedor.getById(20))
+    await contenedor.deleteById(2)
+    console.log( await contenedor.getAll())
+    //await contenedor.deleteAll()
+    
+    console.log( await contenedor.getAll())
+}
+test()
+
+
 
