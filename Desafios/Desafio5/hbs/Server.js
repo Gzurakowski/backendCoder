9ddd8d21914f7a5ac7fd3b4ffcd25a1803ef40a3
gzurakowski <gonchizurak@gmail.com>
2021-10-24 13:24:10 -0300

    Se empieza el desafio 5

---
+++ Desafios/Desafio5/hbs/Server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const app = express()
+
+
+app.use(express.urlencoded({extended: true}))
+
+app.set('views', './views')
+app.set('view engine', 'hbs')
+
+
+
+
+/* ------------------------------------------------------ */
+/* Server Listen */
+const PORT = 8080
+const server = app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${server.address().port}`)
+})
+server.on('error', error => console.log(`Error en servidor error`))
