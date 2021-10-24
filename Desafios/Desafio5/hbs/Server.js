c9c76b9f8bcc77f70e2d2120fdfe03a6215a5f76
gonzalo zurakowski <56240292+Gzurakowski@users.noreply.github.com>
2021-10-24 19:53:00 -0300

    Se hace la plantilla para handlebars (probar)

--- Desafios/Desafio5/hbs/Server.js
+++ Desafios/Desafio5/hbs/Server.js
@@ -1,14 +1,30 @@
 const express = require('express')
-
+const handlebars = require('express-handlebars')
 const app = express()
-
+const productos = []
 
 app.use(express.urlencoded({extended: true}))
 
 app.set('views', './views')
 app.set('view engine', 'hbs')
 
+app.engine('hbs', handlebars({
+    extname:'.hbs',
+    defaultLayout:'index.hbs'
+}))
 
+app.get('/', (req, res) =>{
+    res.render('main', {ruta:''})
+})
+
+app.get('/productos', (req, res) =>{
+    res.render('main', {ruta:'/productos', productos})
+})
+
+app.post('/productos', (req, res) =>{
+    productos.push(req.body)
+    res.redirect('/')
+})
 
 
 /* ------------------------------------------------------ */
