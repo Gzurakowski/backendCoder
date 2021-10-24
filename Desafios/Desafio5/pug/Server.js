c7fa046c47a0b68ed7cb695866c77137ef4a5327
gonzalo zurakowski <56240292+Gzurakowski@users.noreply.github.com>
2021-10-24 20:30:28 -0300

    Se hace el desafio con pug

--- Desafios/Desafio5/pug/Server.js
+++ Desafios/Desafio5/pug/Server.js
@@ -1,14 +1,25 @@
 const express = require('express')
 
 const app = express()
-
+const productos = []
 
 app.use(express.urlencoded({extended: true}))
 
 app.set('views', './views')
 app.set('view engine', 'pug')
 
+app.get('/', (req, res) =>{
+    res.render('main', {ruta:''})
+})
 
+app.get('/productos', (req, res) =>{
+    res.render('main', {ruta:'/productos', productos})
+})
+
+app.post('/productos', (req, res) =>{
+    productos.push(req.body)
+    res.redirect('/')
+})
 
 
 /* ------------------------------------------------------ */
