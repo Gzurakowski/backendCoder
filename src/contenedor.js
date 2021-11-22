295dd2c88224601d4d4afa81fd4361eff680276e
Gonzalo <gzurakowski@loveat.com.ar>
2021-11-22 16:10:24 -0300

    Se arreglan errores

--- src/contenedor.js
+++ src/contenedor.js
@@ -7,7 +7,6 @@ class DB {
         this.tableName = tableName
     }
     crearTabla(){
-        console.log(knex.fn)
         if(this.tableName === 'mensajes'){
             this.db.schema.createTable('mensajes', table =>{
                 table.increments('id')
@@ -61,40 +60,3 @@ class DB {
 }
 
 module.exports = {DB}
-// class Contenedor {
-//     constructor(archivo){
-//         this.id = 0
-//         this.archivo = archivo;
-//     }
-//     async save(data){
-//         data.id = this.id
-//         await fs.promises.readFile(this.archivo,'utf-8')
-//         .then(arch => JSON.parse(arch))// si lee el archivo lo parsea
-//         .then(arch => {
-//             if (arch.length != 0){
-//                 data.id = arch[arch.length - 1].id + 1;
-//                 this.id = data.id;
-//             }
-//             arch.push(data);
-//             return arch;
-//         })// si hay un array de objetos agrega el nuevo objeto
-//         .then(arch => {
-//             fs.promises.writeFile(this.archivo, JSON.stringify(arch)).catch(error =>console.log(error))
-//         })// y vuelve a escribir el archivo
-//         .catch(error => {
-//             console.log(error);
-//             fs.promises.writeFile(this.archivo, JSON.stringify([data])).catch(err => console.log(err));
-//             console.log('se creo el archivo')
-//             this.id = null;        
-//         })
-//         return this.id;
-//     }
-
-    
-//     async getAll(){
-//         return await fs.promises.readFile(this.archivo,'utf-8')
-//         .then(arch => JSON.parse(arch))// si lee el archivo lo parsea
-//         .catch(error => console.log(error))
-//     }
-    
-// }
